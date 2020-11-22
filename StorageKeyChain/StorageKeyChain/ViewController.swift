ddd48351d3ec28a56ec9eb943d8c18b7a670ac10
Champ <champ@odds.team>
2020-11-22 15:26:24 +0700

    keychain

--- StorageKeyChain/StorageKeyChain/ViewController.swift
+++ StorageKeyChain/StorageKeyChain/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         
         save(item: "polygo")
         read()
+        update(password: "hello")
+        read()
+        delete()
+        read()
 //        let result = ref as! NSDictionary
 //        print("result")
 //        result.forEach { key, value in
@@ -27,7 +31,7 @@ class ViewController: UIViewController {
 //
 //        }
     }
-    //SecItemAdd
+    //SecItemAdd -> save ของลง keychain
     private func save(item: String) {
         let keychainItemQuery = [
             kSecValueData: item.data(using: .utf8)!,
@@ -47,7 +51,7 @@ class ViewController: UIViewController {
         print("Operation status :: \(status)")
     }
 
-    
+    //SecItemCopyMatching -> read ของใน keychain
     private func read() {
         let query = [
             kSecClass: kSecClassInternetPassword,
@@ -60,12 +64,42 @@ class ViewController: UIViewController {
         let status = SecItemCopyMatching(query, &result)
         print("Read Operation Status :: \(status)")
         
+        //errSecSuccess คือ status code = 0
+        guard status == errSecSuccess else {
+            print("haven't Item in keychain")
+            return }
         let dic = result as! NSDictionary
         let username = dic[kSecAttrAccount]!
         let passwordData = dic[kSecValueData] as! Data
         let password = String(data: passwordData, encoding: .utf8)!
         
         print("username : \(username) \npassword : \(password)")
+    }
+    
+    //SecItemUpdate -> update field ใน keychain
+    private func update(password: String) {
+        let query = [
+            kSecClass: kSecClassInternetPassword,
+            kSecAttrServer: "google.com"
+        ] as CFDictionary
+        
+        let queryUpdate = [
+            kSecValueData: password.data(using: .utf8)!
+        ] as CFDictionary
+        
+        let status = SecItemUpdate(query, queryUpdate)
+        
+        print("update operation status :: \(status)")
+    }
+    
+    //SecItemDelete -> ลบ item ใน keychain
+    private func delete() {
+        let query = [
+            kSecClass: kSecClassInternetPassword,
+            kSecAttrServer: "google.com"
+        ] as CFDictionary
+        let status = SecItemDelete(query)
+        print("Delete operation status :: \(status)")
     }
 
 }
